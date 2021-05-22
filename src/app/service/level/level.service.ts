41b14e57705c882fd354baf8e5f0777858dec84e
dqa97 <72239024+dqa97@users.noreply.github.com>
2021-05-23 04:59:26 +0700

    viết API cho level, quiz, topic Service

--- src/app/service/level/level.service.ts
+++ src/app/service/level/level.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Level} from '../../model/level';
+import {Observable} from 'rxjs';
 
+const API_URL = `${environment.apiUrl}`
 @Injectable({
   providedIn: 'root'
 })
 export class LevelService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  listLevel(level: Level): Observable<Level[]> {
+    return this.http.get<Level[]>(`${API_URL}/levels`);
+  }
+
+  getLevelsByTopic(id: number): Observable<Level[]> {
+    return this.http.get<Level[]>(`${API_URL}/levels/GetLevelByTopic/${id}`)
+  }
 }
