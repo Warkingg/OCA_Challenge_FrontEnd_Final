89f0939a3e74917aa3ba99bd8a18a59e17d76b93
Tam250399 <tamthanhsk25@gmail.com>
2021-05-23 10:47:59 +0700

    add-home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LevelComponent } from './level/level.component';
+import { QuestionComponent } from './question/question.component';
+import { AnswerComponent } from './answer/answer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LevelComponent,
+    QuestionComponent,
+    AnswerComponent
   ],
   imports: [
     BrowserModule,
