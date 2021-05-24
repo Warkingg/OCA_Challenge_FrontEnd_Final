5533f4aa81ac3c2121d02b10f7231bac67da490c
Warkingg <75408449+Warkingg@users.noreply.github.com>
2021-05-24 16:24:30 +0700

    Delete CorrectAnswer & History Interface, Service and Add some attributes to Answer & Question interface

--- src/app/model/answer.ts
+++ src/app/model/answer.ts
@@ -2,4 +2,6 @@ export interface Answer {
   id?: number;
   content?: string;
   question?: any;
+  isAnswer?: boolean;
+  selected?: boolean;
 }
--- src/app/model/question.ts
+++ src/app/model/question.ts
@@ -5,4 +5,5 @@ export interface Question {
   questionType ?: any;
   level ?: any;
   quiz ?: any;
+  answered?: boolean;
 }
